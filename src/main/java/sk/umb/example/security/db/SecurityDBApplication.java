992b616e69d43cc8353a89ade1ef1a71a4ad5275
Robert Gallas <gallas.robert@gmail.com>
2022-04-05 22:20:07 +0200

    Package name udpate

--- src/main/java/sk/umb/example/security/db/SecurityDBApplication.java
+++ src/main/java/sk/umb/example/security/db/SecurityDBApplication.java
@@ -1,4 +1,4 @@
-package sk.umb.example.library;
+package sk.umb.example.security.db;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
