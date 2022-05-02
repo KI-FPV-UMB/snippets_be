57333d0775da5f800553d283b92581e61eb0d047
Robert Gallas <gallas.robert@gmail.com>
2022-05-02 23:02:58 +0200

    Small fixes

--- src/main/java/sk/umb/example/validation/book/service/BookService.java
+++ src/main/java/sk/umb/example/validation/book/service/BookService.java
@@ -3,12 +3,9 @@ package sk.umb.example.validation.book.service;
 import com.github.javafaker.Faker;
 import org.springframework.stereotype.Service;
 
-import java.util.Random;
-
 @Service
 public class BookService {
     private static Faker FAKER = new Faker();
-    private static Random RANDOM_NUMBERS = new Random();
 
     public BookResponseDto getBook(Long bookId) {
         if (bookId != 1) {
@@ -26,6 +23,6 @@ public class BookService {
     }
 
     public Long createBook(BookCreateDto bookCreateDto) {
-        return RANDOM_NUMBERS.nextLong();
+        return 2L;
     }
 }
