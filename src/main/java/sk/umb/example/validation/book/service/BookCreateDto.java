8be582b55769841c1118604ab1b84b67ce3b399a
Robert Gallas <gallas.robert@gmail.com>
2022-05-02 23:04:23 +0200

    Cleanup

--- src/main/java/sk/umb/example/validation/book/service/BookCreateDto.java
+++ src/main/java/sk/umb/example/validation/book/service/BookCreateDto.java
@@ -1,8 +1,5 @@
 package sk.umb.example.validation.book.service;
 
-import org.springframework.validation.annotation.Validated;
-
-import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
