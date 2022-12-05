e4cd2a16c51f23d18da7da29a1a190048e3a0936
Robert Gallas <gallas.robert@gmail.com>
2022-12-05 13:11:56 +0100

    Message counter

--- worker/src/main/java/com/example/rabbitmq/worker/Receiver.java
+++ worker/src/main/java/com/example/rabbitmq/worker/Receiver.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Receiver {
+    private int counter  = 0;
     public void receiveMessage(String message) {
-        System.out.println(WorkerApplication.APP_NAME + " received message: " + message);
+        System.out.println(WorkerApplication.APP_NAME + " received message #" + counter++ +": " + message);
     }
 }
