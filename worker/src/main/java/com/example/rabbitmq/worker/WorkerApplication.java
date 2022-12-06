4a86078715d9abdbd47045fb752a48f9c3f80120
Robert Gallas <gallas.robert@gmail.com>
2022-12-06 14:54:19 +0100

    Fix

--- worker/src/main/java/com/example/rabbitmq/worker/WorkerApplication.java
+++ worker/src/main/java/com/example/rabbitmq/worker/WorkerApplication.java
@@ -1,7 +1,9 @@
 package com.example.rabbitmq.worker;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WorkerApplication {
@@ -15,4 +17,13 @@ public class WorkerApplication {
 
         new Subscriber().subscribe();
     }
+
+    // ----- Configuration -----
+    private String queueName = "DEMO-QUEUE";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName);
+    }
+
 }
